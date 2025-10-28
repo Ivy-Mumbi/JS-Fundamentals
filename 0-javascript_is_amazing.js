ed1872e8e1c5fee247c29260f63ffc6133b36965
Ivy-Mumbi <ivy.mumbi.5@gmail.com>
2025-10-28 22:42:33 +0300

    First commit

---
+++ 0-javascript_is_amazing.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// Create a constant variable named myVar with the specified value
+const myVar = "JavaScript is amazing";
+
+// Print the value of the variable to the console
+console.log(myVar);
