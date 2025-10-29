3eab50e81a34e927b56b2adcde28639ba4140d5f
Ivy-Mumbi <ivy.mumbi.5@gmail.com>
2025-10-30 00:16:24 +0300

    Add multi-languages

---
+++ 1-multi_languages.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Using console.log() to print each line as requested
+
+// The first line
+console.log("C is fun");
+
+// The second line
+console.log("Python is cool");
+
+// The third line
+console.log("JavaScript is amazing");
