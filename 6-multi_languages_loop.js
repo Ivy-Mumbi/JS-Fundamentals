e29fe80e38f340624f206e04e01b3971bff9b79a
Ivy-Mumbi <ivy.mumbi.5@gmail.com>
2025-10-30 00:31:56 +0300

    Commit 6

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// Create an array of the strings to be printed
+const lines = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
+
+// Loop through each element in the array
+for (const line of lines) {
+  // Print the current element
+  console.log(line);
+}
